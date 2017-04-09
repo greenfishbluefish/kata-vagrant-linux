d49e8e1248330b40bb06dd6852c513aa5af4c2b2
Rob Kinyon <rob.kinyon@gmail.com>
2017-04-09 14:47:28 -0400

    Add comments to sample script

--- devops/provisioning/scripts/script1.sh
+++ devops/provisioning/scripts/script1.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-set -e
+# Notes:
+#   * This script (like all provisioners) runs as root. So, no need to "sudo".
+#   * All actions in this script are idempotent. This means the same end result
+#     no matter how many times it is run. q.v. README.md for more discussion.
+
+# Use good Bash practices. For more information, q.v.
+#   * https://google.github.io/styleguide/shell.xml
+set -eu
 
 echo "script1 ran"
